912e87fb9fba0d13b4c579bacd0d2aa4938a6e82
ChristianABoesch <cab.zander@gmail.com>
2020-04-18 22:37:34 -0700

    Adding buttons and a select menu
    This app is coming along ... so far we have the visual technology we need, now waiting to input speech.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
     <h1>WE ARE HERE.</h1>
+    <select v-model="selected">
+      <option disabled value>Please select one</option>
+      <option>Joanna</option>
+      <option>Matthew</option>
+      <option>Watibushawa</option>
+    </select>
     <input v-model="newDialog">
     <button @click="addDialog">Add dialogue</button>
-    <ol>
-      <li v-for="dialog in dialogs">{{dialog}}</li>
-    </ol>
+    <button @click="dialogs=[]">Reset</button>
+    <button @click="fixit('Hey Papi, this needs to be implemented.')">Play All</button>
+
+    <ul>
+      <li v-for="dialog in dialogs">
+        <button @click="fixit('Hey Papi, this needs to be implemented.')">Play</button>
+        <span>{{dialog}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,6 +26,7 @@ export default {
   name: "App",
   data() {
     return {
+      selected: "Joanna",
       newDialog: "Y-yo.",
       dialogs: ["I like frogs", "Look at the time!", "This party's over"]
     };
@@ -23,18 +36,10 @@ export default {
       //alert("Hey Papi, can you make this thing work?")
       this.dialogs.push(this.newDialog);
       this.newDialog = "";
+    },
+    fixit(message) {
+      alert(message);
     }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
